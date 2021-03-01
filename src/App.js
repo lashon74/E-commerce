dba930dc3ec6d43e86b5fa79ad53f0cc6c0e743a
Lashon Hudson <lashonh74@gmail.com>
2021-02-28 19:50:53 -0500

    Added Mock Products

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Products from './components/Products/Products'
+
+const App = () => {
+  return (
+    <div>
+      <h1>E-commerce</h1>
+      <Products/>
+
+    </div>
+  )
+}
+
+export default App
+
---
+++ src/components/Products/Products.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import {Grid} from "@material-ui/core";
+
+import Product from "./Product/Product";
+// import useStyles from "./styles";
+
+const products = [
+  {
+    id: 1,
+    name: "Shoes",
+    decription: "Running shoes.",
+    price: "$5",
+    image:
+      "https://images.unsplash.com/photo-1589187832032-3e560ed4e6b9?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Nnx8cnVubmluZyUyMHNob2VzfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+  },
+  {
+    id: 2,
+    name: "Macbook",
+    decription: "Apple macbook.",
+    price: "$500",
+    image:
+      "https://images.unsplash.com/photo-1531934788018-04c3cd417b80?ixid=MXwxMjA3fDB8MHxzZWFyY2h8N3x8bWFjYm9va3xlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+  },
+];
+
+const Products = () => {
+  // const classes = useStyles();
+
+  // if (!products.length) return <p>Loading...</p>;
+
+  return (
+    // <main className={classes.content}>
+    <main>
+      {/* <div className={classes.toolbar} /> */}
+      <Grid container justify="center" spacing={4}>
+        {products.map((product) => (
+          <Grid key={product.id} item xs={12} sm={6} md={4} lg={3}>
+            <Product product={product} />
+          </Grid>
+        ))}
+      </Grid>
+    </main>
+  );
+};
+
+export default Products;
