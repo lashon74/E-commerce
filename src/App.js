4aff1cfb62b5be992193d5b0ddd1d2e22a3bc94e
Lashon Hudson <lashonh74@gmail.com>
2021-03-01 23:42:01 -0500

    Added cart view page and react router dom

--- src/App.js
+++ src/App.js
@@ -1,33 +1,46 @@
-import React,{useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { commerce } from "./lib/commerce";
-import { Navbar, Products } from "./components";
+import { Navbar, Products, Cart } from "./components";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 const App = () => {
   const [products, setProducts] = useState([]);
-  const [cart, setCart] = useState({})
+  const [cart, setCart] = useState({});
 
   const fetchProducts = async () => {
-    const {data} = await commerce.products.list();
+    const { data } = await commerce.products.list();
 
     setProducts(data);
-  }
+  };
   const fatchCart = async () => {
-    const cart = await commerce.cart.retrieve();
+    setCart(await commerce.cart.retrieve());
+  };
 
-    setCart(cart);
-  }
+  const handleAddTooCart = async (productId, quanitiy) => {
+    const item = await commerce.cart.add(productId, quanitiy);
+
+    setCart(item.cart);
+  };
 
   useEffect(() => {
     fetchProducts();
     fatchCart();
-  }, [])
-
+  }, []);
+  console.log(cart);
   return (
-    <div>
-      <h1>E-commerce</h1>
-      <Navbar />
-      <Products products={products}/>
-    </div>
+    <Router>
+      <div>
+        <Navbar totalItems={cart.total_items} />
+        <Switch>
+          <Route exact path="/">
+            <Products products={products} onAddToCart={handleAddTooCart} />
+          </Route>
+          <Route exact path="/cart">
+            <Cart cart={cart} />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 };
 
--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -11,7 +11,7 @@ import { AddShoppingCart } from "@material-ui/icons";
 
 import useStyles from "./styles";
 
-const Product = ({ product }) => {
+const Product = ({ product, onAddToCart }) => {
   const classes = useStyles();
   return (
     <Card className={classes.root}>
@@ -36,7 +36,10 @@ const Product = ({ product }) => {
         />
       </CardContent>
       <CardActions disableSpacing className={classes.cardActions}>
-        <IconButton aria-label="Add to cart">
+        <IconButton
+          aria-label="Add to cart"
+          onClick={() => onAddToCart(product.id, 1)}
+        >
           <AddShoppingCart />
         </IconButton>
       </CardActions>
