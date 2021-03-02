24f070035dab3ccd1bd7b0ae154537171580d62e
Lashon Hudson <lashonh74@gmail.com>
2021-03-01 22:05:14 -0500

    Added commerce.js and added products to homescreen

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-import React from "react";
+import React,{useState, useEffect} from "react";
+import { commerce } from "./lib/commerce";
 import { Navbar, Products } from "./components";
 
 const App = () => {
+  const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState({})
+
+  const fetchProducts = async () => {
+    const {data} = await commerce.products.list();
+
+    setProducts(data);
+  }
+  const fatchCart = async () => {
+    const cart = await commerce.cart.retrieve();
+
+    setCart(cart);
+  }
+
+  useEffect(() => {
+    fetchProducts();
+    fatchCart();
+  }, [])
+
   return (
     <div>
       <h1>E-commerce</h1>
       <Navbar />
-      <Products />
+      <Products products={products}/>
     </div>
   );
 };
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -3,26 +3,7 @@ import {Grid} from "@material-ui/core";
 import Product from "./Product/Product";
 import useStyles from "./styles";
 
-const products = [
-  {
-    id: 1,
-    name: "Shoes",
-    decription: "Running shoes.",
-    price: "$5",
-    image:
-      "https://images.unsplash.com/photo-1589187832032-3e560ed4e6b9?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Nnx8cnVubmluZyUyMHNob2VzfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
-  },
-  {
-    id: 2,
-    name: "Macbook",
-    decription: "Apple macbook.",
-    price: "$500",
-    image:
-      "https://images.unsplash.com/photo-1531934788018-04c3cd417b80?ixid=MXwxMjA3fDB8MHxzZWFyY2h8N3x8bWFjYm9va3xlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
-  },
-];
-
-const Products = () => {
+const Products = ({products}) => {
   const classes = useStyles();
 
   // if (!products.length) return <p>Loading...</p>;
