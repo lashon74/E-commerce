2a6e7aaa721dfe5069defd0b422fa8e602498a9f
Lashon Hudson <lashonh74@gmail.com>
2021-03-03 19:34:50 -0500

    Creates a checkout pages using react-forms and commerse js

--- src/components/CheckoutForm/Checkout/Checkout.jsx
+++ src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -6,44 +6,107 @@ import {
   Step,
   StepLabel,
   Typography,
-  //   CircularProgress,
-  //   Divider,
-  //   Button,
+  CircularProgress,
+  Divider,
+  Button,
 } from "@material-ui/core";
 import { Link, useHistory } from "react-router-dom";
-// import { commerce } from "../../../lib/commerce";
+
+import { commerce } from "../../../lib/commerce";
 import AddressForm from "../AddressForm";
 import PaymentForm from "../PaymentForm";
 import useStyles from "./styles";
 
 const steps = ["Shipping address", "Payment details"];
 
-const Checkout = () => {
+const Checkout = ({ cart, onCaptureCheckout, order, error }) => {
+  const [checkoutToken, setCheckoutToken] = useState(null);
   const [activeStep, setActiveStep] = useState(0);
+  const [shippingData, setShippingData] = useState({});
   const classes = useStyles();
+  const history = useHistory();
 
-    const Confirmation = () => (
+  const nextStep = () => setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  const backStep = () => setActiveStep((prevActiveStep) => prevActiveStep - 1);
+
+  useEffect(() => {
+    if (cart.id) {
+      const generateToken = async () => {
+        try {
+          const token = await commerce.checkout.generateToken(cart.id, {
+            type: "cart",
+          });
+
+          setCheckoutToken(token);
+        } catch {
+          if (activeStep !== steps.length) history.push("/");
+        }
+      };
+
+      generateToken();
+    }
+  }, [cart]);
+
+  const test = (data) => {
+    setShippingData(data);
+
+    nextStep();
+  };
+
+  let Confirmation = () =>
+    order.customer ? (
+      <>
         <div>
-            Confirmation
+          <Typography variant="h5">
+            Thank you for your purchase, {order.customer.firstname}{" "}
+            {order.customer.lastname}!
+          </Typography>
+          <Divider className={classes.divider} />
+          <Typography variant="subtitle2">
+            Order ref: {order.customer_reference}
+          </Typography>
         </div>
-    )
+        <br />
+        <Button component={Link} variant="outlined" type="button" to="/">
+          Back to home
+        </Button>
+      </>
+    ) : (
+      <div className={classes.spinner}>
+        <CircularProgress />
+      </div>
+    );
+
+  if (error) {
+    Confirmation = () => (
+      <>
+        <Typography variant="h5">Error: {error}</Typography>
+        <br />
+        <Button component={Link} variant="outlined" type="button" to="/">
+          Back to home
+        </Button>
+      </>
+    );
+  }
+
   const Form = () =>
     activeStep === 0 ? (
       <AddressForm
-      //   checkoutToken={checkoutToken}
-      //   nextStep={nextStep}
-      //   setShippingData={setShippingData}
-      //   test={test}
+        checkoutToken={checkoutToken}
+        nextStep={nextStep}
+        setShippingData={setShippingData}
+        test={test}
       />
     ) : (
       <PaymentForm
-      //   checkoutToken={checkoutToken}
-      //   nextStep={nextStep}
-      //   backStep={backStep}
-      //   shippingData={shippingData}
-      //   onCaptureCheckout={onCaptureCheckout}
+        checkoutToken={checkoutToken}
+        nextStep={nextStep}
+        backStep={backStep}
+        shippingData={shippingData}
+        onCaptureCheckout={onCaptureCheckout}
       />
     );
+
   return (
     <>
       <CssBaseline />
@@ -60,7 +123,11 @@ const Checkout = () => {
               </Step>
             ))}
           </Stepper>
-          {activeStep === steps.length ? <Confirmation /> : <Form />}
+          {activeStep === steps.length ? (
+            <Confirmation />
+          ) : (
+            checkoutToken && <Form />
+          )}
         </Paper>
       </main>
     </>
